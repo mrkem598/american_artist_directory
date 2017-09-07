46f428408ff136b6bc448e82fce4563be4f57ab1
Moh Kemal <mohakemal9@gmail.com>
2017-09-07 19:14:14 -0400

    template added

--- app/component.app.ts
+++ app/component.app.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 //component decorator & define what this componenet will do//
 @Component({
   selector: 'app',
-  template: '<h1>American Artist Directory</h1>'
+  templateUrl: './partials/app.html'
 })
 
 export class AppComponent {}
